8c600fa89e28ee026e72f649956c9c624960c491
Atharva Bhoite <105184536+atharva7803@users.noreply.github.com>
2025-01-27 19:22:34 +0530

    Add files via upload

---
+++ Assignments/Assignment 12/Question12_5.c
@@ -0,0 +1,27 @@
+// 5. Accept number from user and display below pattern.
+
+// Input: 8
+// Output: 2   4   6   8   10  12  14  16
+
+#include<stdio.h>
+
+void Display(int iNo){
+    int iCnt = 0;
+    for(iCnt = 1; iCnt <= iNo; iCnt++){     
+        printf("%d\t",iCnt * 2);
+    }
+    printf("\n");
+}
+
+
+int main(){
+
+    int iValue = 0;
+    
+    printf("Enter number of elements: \n");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
