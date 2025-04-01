71f4c6e89f6da8a0fce505478248cd95d7262fb9
Atharva Bhoite <105184536+atharva7803@users.noreply.github.com>
2025-04-01 11:54:56 +0530

    Add files via upload

---
+++ Assignments/Assignment 33 Recursion-1/Question33_1.cpp
@@ -0,0 +1,30 @@
+// 1. Write a recursive program which display below pattern.
+// Input: 5
+// Output: *   *   *   *   *
+
+
+#include<iostream>
+using namespace std;
+
+void Display(int iNo){
+    static int i = 0;            
+    if(i < iNo){
+        cout <<"*\t";
+        i++;
+        Display(iNo);
+    }
+}
+
+int main(){
+
+    int iValue = 0;
+
+    cout<<"Enter number: "<<endl;
+    cin>>iValue;
+
+    Display(iValue);
+
+    cout<<"\nEnd of application!"<<endl;
+
+    return 0;
+}
