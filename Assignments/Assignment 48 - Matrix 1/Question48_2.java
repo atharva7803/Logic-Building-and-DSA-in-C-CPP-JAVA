1177f6f2af4ffc69f4e7d9e55c84600a7a51d347
Atharva Bhoite <105184536+atharva7803@users.noreply.github.com>
2025-06-27 19:41:41 +0530

    Add files via upload

---
+++ Assignments/Assignment 48 - Matrix 1/Question48_2.java
@@ -0,0 +1,99 @@
+/*
+
+2. Write a program which accept matrix from user and return frequency of that number.
+
+Input:
+
+Number: 9
+
+3   2   5   9
+4   3   2   2
+8   4   1   5
+3   9   7   5
+
+Output: 3
+
+*/
+
+
+import java.util.*;
+
+class Matrix{
+    public int iRow,iCol;
+    public int arr[][];
+
+    public Matrix(int a, int b){
+        this.iRow = a;
+        this.iCol = b;
+
+        arr = new int[iRow][iCol];
+    }
+
+    protected void finalize(){
+        System.out.println("Garbage collector is collecting the memory of an object!");
+        arr = null;
+    }
+
+    public void Accept(){
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("Enter values: ");
+
+        for(int i=0; i<iRow; i++) {
+            for(int j=0; j<iCol; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+    }
+
+    public void Display(){
+        System.out.println("Elements from the matrix are: ");
+        for(int i=0; i<iRow; i++) {
+            for(int j=0; j<iCol; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+    public int Frequency(int no){
+        int iCnt = 0;
+        for(int i=0; i<iRow; i++) {
+            for(int j=0; j<iCol; j++) {
+                if(arr[i][j] == no)
+                    iCnt++;
+            }
+        }
+        return iCnt;
+    }
+
+}
+
+class Question48_2 {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); 
+        
+        System.out.println("Enter number of rows: ");
+        int no1 = sc.nextInt();
+
+        System.out.println("Enter number of columns: ");
+        int no2 = sc.nextInt();
+
+        Matrix mobj = new Matrix(no1, no2);
+
+        mobj.Accept();
+        mobj.Display();
+
+        System.out.println("Enter number of which you want frequency: ");
+        int no3 = sc.nextInt();
+
+        System.out.println("Frequency of the number is: "+ mobj.Frequency(no3));
+
+        mobj = null;
+
+        System.gc();
+
+    }
+} 
