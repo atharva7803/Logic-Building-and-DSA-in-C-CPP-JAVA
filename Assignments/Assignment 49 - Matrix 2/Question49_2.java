df92c40008efd8faff1263f13f2121773770d603
Atharva Bhoite <105184536+atharva7803@users.noreply.github.com>
2025-07-01 14:11:57 +0530

    Add files via upload

---
+++ Assignments/Assignment 49 - Matrix 2/Question49_2.java
@@ -0,0 +1,106 @@
+/*
+
+2. Write a program which accept matrix and reverse the contents of each row.
+
+
+Input:
+
+3   2   5   9
+4   3   2   2
+8   4   1   5
+3   9   7   5
+
+Output: 
+
+9   5   2   3   
+2   2   3   4   
+9   1   4   8
+5   7   9   3
+
+*/
+
+
+import java.util.*;
+
+class Matrix{
+    public int iRow,iCol;
+    public int arr[][];
+
+    public Matrix(int a, int b){
+        this.iRow = a;
+        this.iCol = b;
+
+        arr = new int[iRow][iCol];
+    }
+
+    protected void finalize(){
+        System.out.println("Garbage collector is collecting the memory of an object!");
+        arr = null;
+    }
+
+    public void Accept(){
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("Enter values: ");
+
+        for(int i=0; i<iRow; i++) {
+            for(int j=0; j<iCol; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+    }
+
+    public void Display(){
+        System.out.println("Elements from the matrix are: ");
+        for(int i=0; i<iRow; i++) {
+            for(int j=0; j<iCol; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+    public void ReverseRow(){
+        for (int i = 0; i < iRow; i++) {
+            int start = 0;
+            int end = iCol - 1;
+            while (start < end) {
+                // Swap arr[i][start] and arr[i][end]
+                int temp = arr[i][start];
+                arr[i][start] = arr[i][end];
+                arr[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+
+}
+
+class Question49_2 {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); 
+        
+        System.out.println("Enter number of rows: ");
+        int no1 = sc.nextInt();
+
+        System.out.println("Enter number of columns: ");
+        int no2 = sc.nextInt();
+
+        Matrix mobj = new Matrix(no1, no2);
+
+        mobj.Accept();
+        mobj.Display();
+
+        System.out.println("After reversing row: ");
+
+        mobj.ReverseRow();
+        mobj.Display();
+
+        mobj = null;
+        System.gc();
+
+    }
+}  
